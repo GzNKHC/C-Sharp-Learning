db381ab5f62943501c7a1ed798b26d0b7f2fee59
Nguyen Khai Hoan Ca <phuchungca@gmail.com>
2026-09-08 19:48:02 +0700

    Update ex1

---
+++ Lab01/Lesson 1/Ex01.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("""
+*******************************
+* Truong: Dai Hoc Sai Gon     *
+* Khoa: CNTT                  *
+* Ho ten: Nguyen Khai Hoan Ca *
+*******************************
+""");
